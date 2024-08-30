ea405b04b1ba149ddf718d16cd141f8aa4fdd8a9
maheshguptha49 <maheshguptha49@gmail.com>
2024-08-30 09:21:34 +0530

    changed pdf link

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -85,7 +85,7 @@ const Home = ({ scrollRef }) => {
 						<p style={{fontSize:"85%"}}>I build things on Internet</p> */}
 					</h1>
 					<div className={styles.btn}>
-						<a href='https://drive.google.com/file/d/13PeTq922yQ9LVoAnnjnfswvKHCt4C1lr/view?usp=sharing' target="_blank" rel="noreferrer">   
+						<a href='https://drive.google.com/file/d/1-egxGw4vpj_vr233HkmjBkYkiUGvhX8L/view?usp=drive_link' target="_blank" rel="noreferrer">   
                         {/*  add resume here*/}
 							<Button
 								text={
